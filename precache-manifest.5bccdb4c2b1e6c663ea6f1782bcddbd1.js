9384cb5f4bed2658803025afd51741c7b071f82c
alexanderutrobin11 <alexanderutrobin11@gmail.com>
2019-02-21 00:46:44 +0300

    Updates

---
+++ precache-manifest.5bccdb4c2b1e6c663ea6f1782bcddbd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc2e4447ed19a23c5c3f",
+    "url": "/car-price-app/static/js/runtime~main.cc2e4447.js"
+  },
+  {
+    "revision": "ca25ae7a304d12a65d48",
+    "url": "/car-price-app/static/js/main.ca25ae7a.chunk.js"
+  },
+  {
+    "revision": "dd8771b02f967a505332",
+    "url": "/car-price-app/static/js/2.dd8771b0.chunk.js"
+  },
+  {
+    "revision": "ca25ae7a304d12a65d48",
+    "url": "/car-price-app/static/css/main.65df522f.chunk.css"
+  },
+  {
+    "revision": "c271012a29527bba0ea95adeff21b212",
+    "url": "/car-price-app/index.html"
+  }
+];
